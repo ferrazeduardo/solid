cf7a7aea4eccb708ac057746ee2f1e9526e0c51f
Eduardo Borges Ferraz <ebferraz@aslan.br>
2021-07-21 00:28:43 -0300

    primeiro commit

---
+++ src/Alura.LeilaoOnline.WebApp/Dados/EfCore/LeilaoDaoComEfCore.cs
@@ -0,0 +1,58 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.LeilaoOnline.WebApp.Dados.EfCore
+{
+    public class LeilaoDaoComEfCore : ILeilaoDao
+    {
+        AppDbContext _context;
+
+        public LeilaoDaoComEfCore()
+        {
+            _context = new AppDbContext();
+        }
+
+        public IEnumerable<Categoria> ListarCategorias()
+        {
+            return _context.Categorias.ToList();
+        }
+
+        public Leilao BuscarPorId(int id)
+        {
+            return _context.Leiloes.Find(id);
+        }
+
+        public void Excluir(Leilao leilao)
+        {
+            _context.Leiloes.Remove(leilao);
+            _context.SaveChanges();
+        }
+
+        public void Update(Leilao leilao)
+        {
+            _context.Leiloes.Update(leilao);
+            _context.SaveChanges();
+        }
+        public void Inserir(Leilao model)
+        {
+            _context.Leiloes.Add(model);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<Leilao> BuscarLeiloes()
+        {
+            return _context.Leiloes
+                            .Include(l => l.Categoria).ToList();
+        }
+
+        public List<Categoria> ObterCategorias()
+        {
+            return _context.Categorias
+                .Include(c => c.Leiloes).ToList();
+        }
+    }
+}
